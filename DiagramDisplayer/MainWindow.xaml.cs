a4a3f9b732d55bfcf4d0c7c2d8de89fc0cb2b1ff
Tylor Pater <tylorpater@outlook.com>
2017-11-28 20:31:14 -0800

    Add project files.

---
+++ DiagramDisplayer/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace DiagramDisplayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowModel _model = new MainWindowModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _model;
+
+            LoadItems();
+            OverGrid.Visibility = Visibility.Collapsed;
+        }
+
+        public void LoadItems()
+        {
+            LoadForFolder("logical", _model.Logical);
+            LoadForFolder("development", _model.Development);
+            LoadForFolder("physical", _model.Physical);
+            LoadForFolder("process", _model.Process);
+            LoadForFolder("scenarios", _model.Scenarios);
+        }
+
+        public void LoadForFolder(string folderName, ObservableCollection<ImageItem> itemContainer)
+        {
+            var mainpath = System.IO.Path.Combine(Environment.CurrentDirectory, "models", folderName);
+            foreach (var f in new DirectoryInfo(mainpath).GetFiles())
+            {
+                var l = new ImageItem();
+                l.Image = f.FullName;
+                l.Title = f.Name;
+                itemContainer.Add(l);
+            }
+        }
+
+        private void ListBox_Selected(object sender, EventArgs e)
+        {
+            var s = (ListBox)sender;
+            if (s.SelectedItem == null) return;         
+            _model.SelectedItem = (ImageItem)s.SelectedItem;
+            OverGrid.Visibility = Visibility.Visible;
+        }
+
+        private void CloseGrid(object sender, RoutedEventArgs e)
+        {
+            OverGrid.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ DiagramDisplayer/MainWindowModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramDisplayer
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<ImageItem> Logical { get; set; } = new ObservableCollection<ImageItem>();
+        public ObservableCollection<ImageItem> Development { get; set; } = new ObservableCollection<ImageItem>();
+        public ObservableCollection<ImageItem> Process { get; set; } = new ObservableCollection<ImageItem>();
+        public ObservableCollection<ImageItem> Physical { get; set; } = new ObservableCollection<ImageItem>();
+        public ObservableCollection<ImageItem> Scenarios { get; set; } = new ObservableCollection<ImageItem>();
+
+        ImageItem _selectedItem;
+        public ImageItem SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedItem")); }
+        }
+    }
+
+    public class ImageItem
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+    }
+}
